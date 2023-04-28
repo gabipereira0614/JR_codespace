8d6e8362e7d97cc5a2e9d3d2a5e35b49f0e1e70f
Nicolas <101908083+NicolasSamuel@users.noreply.github.com>
2023-04-28 12:52:46 +0000

    login

--- app/Views/admin/produtos.php
+++ app/Views/admin/produtos.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,10 +8,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Document</title>
 </head>
+
 <body>
     <main class="container-produto">
         <h1>gerenciar produtos</h1>
-        <?= form_open(base_url("/admin/Produto/cadastrar"))?>
+        <?= form_open(base_url("/admin/Produto/cadastrar")) ?>
         <div class="mb-3">
             <label for="idproduto" class="form-label">Código do produto</label>
             <input type="text" class="form-control" id="idproduto" name="idproduto" readonly>
@@ -36,18 +38,19 @@
                 </tr>
             </thead>
             <tbody>
-            <?php foreach($produtos as $produto): ?>
-                <tr>
-                    <th scope="row"><?= $produto["idproduto"] ?></th>
-                    <td><?= $produto["nome"] ?></td>
-                    <td><?= $produto["preco"] ?></td>
-                    <td>EXCLUIR</td>
-                </tr>
-            <?php endforeach;?>
+                <?php foreach ($produtos as $produto) : ?>
+                    <tr>
+                        <th scope="row"><?= $produto["idproduto"] ?></th>
+                        <td><?= $produto["nome"] ?></td>
+                        <td><?= $produto["preco"] ?></td>
+                        <td>EXCLUIR</td>
+                    </tr>
+                <?php endforeach; ?>
             </tbody>
-        </div>
+            </div>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 </body>
+
 </html>
