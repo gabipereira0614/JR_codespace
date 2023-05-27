570aadc07a5bd1bf171fda2e80ad3863947c8788
Alan Davi <101908061+Mojiozin@users.noreply.github.com>
2023-05-27 01:48:33 +0000

    Correção de codigo

--- app/Views/portifolio.php
+++ app/Views/portifolio.php
@@ -10,19 +10,10 @@
     <div class="titulo-portifolio">
         <h2>Nossos Destaques</h2>
     </div>
-<<<<<<< HEAD
     <div class="container-portifolio">
             <?php foreach ($portifolios as $portifolio) : ?>
                 <img src="/uploads/portifolio/<?= $portifolio["imagem"] ?>" alt="Imagem de Portifolio" class="imagem">
             <?php endforeach; ?>
-=======
-    <div class="container text-center">
-        <?php foreach ($portifolios as $portifolio) : ?>
-            <div class="imagens-portifolio" class="col">
-                <img src="/uploads/portifolio/<?= $portifolio["imagem"] ?>" alt="Imagem de Portifolio" class="imagem">
-            </div>
-        <?php endforeach; ?>
->>>>>>> 1ec5787e19fa3e4750a702a290c375a525d3e1f0
     </div>
 </div>
 
